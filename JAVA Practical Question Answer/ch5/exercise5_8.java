993c2a7b44a7e7a1f48bdafd4467bdd5bc7938dc
이재원 <khbank.ubuntu@gmail.com>
2018-01-10 01:05:53 +0900

    ch5
    8

---
+++ JAVA Practical Question Answer/ch5/exercise5_8.java
@@ -0,0 +1,17 @@
+public class exercise5_8 {
+    public static void main(String[] args) {
+        int[] answer = {1,4,4,3,1,4,4,2,1,3,2};
+        int[] counter = new int[4];
+
+        for(int i:answer)
+            counter[i-1]++;
+
+        for (int i:counter) {
+            System.out.print(i);
+            for (int j=1;j<=i;j++)
+                System.out.print('*');
+
+            System.out.println();
+        }
+    }
+}
