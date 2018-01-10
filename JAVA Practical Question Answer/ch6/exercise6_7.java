876159683b079faabd70bbc76810e8460dbee287
이재원 <khbank.ubuntu@gmail.com>
2018-01-11 02:35:16 +0900

    ch6
    7

---
+++ JAVA Practical Question Answer/ch6/exercise6_7.java
@@ -0,0 +1,21 @@
+class MyPoint{
+    int x;
+    int y;
+
+    MyPoint(int x, int y) {
+        this.x=x;
+        this.y=y;
+    }
+
+    double getDistance(int x, int y) {
+        return Math.sqrt((x-this.x)*(x-this.x)+(y-this.y)*(y-this.y));
+    }
+}
+
+public class exercise6_7 {
+    public static void main(String[] args) {
+        MyPoint p = new MyPoint(1,1);
+
+        System.out.println(p.getDistance(2,2));
+    }
+}
